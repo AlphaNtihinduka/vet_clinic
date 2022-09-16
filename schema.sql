3fbfbec67b10e2b30e58f3ae6d0bac33441290a8
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-09-16 19:14:19 +0300

    create owner and species table

--- schema.sql
+++ schema.sql
@@ -14,3 +14,28 @@ weight_kg DECIMAL);
 -- update colums
 ALTER TABLE animals 
 ADD species varchar(30);
+
+-- Create table owners
+CREATE TABLE owners(
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+full_name varchar(40),
+age INT);
+
+-- create table species
+CREATE TABLE species(
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+name varchar(40));
+
+-- remove column species
+ALTER TABLE animals
+DROP COLUMN species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD FOREIGN KEY(species_id) REFERENCES species(id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD FOREIGN KEY(species_id) REFERENCES owners(id);
